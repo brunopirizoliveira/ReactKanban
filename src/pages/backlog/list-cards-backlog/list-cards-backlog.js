7a55b73a50dbadd2b3f90c487197eae9074afa97
brunooliverdwl <bruno.pires@qualitor.com.br>
2019-07-24 16:51:41 -0300

    Utilização de state e fetch

--- src/pages/backlog/list-cards-backlog/list-cards-backlog.js
+++ src/pages/backlog/list-cards-backlog/list-cards-backlog.js
@@ -4,9 +4,38 @@ import './styles.css';
 
 export default class ListCardsBacklog extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            lista: []
+        };
+    }
+
+    // Antes do Component ser montado
+    componentWillMount() {
+        fetch('http://wpoa000939:3000/users', { method: 'GET'})
+        .then(responseUsers => responseUsers.json())
+        .then(dataUsers => this.setState({lista: dataUsers.recordset}))
+     }
+
+    // Component acabou de ser montado
+    // componentDidMount() { }
+
     render() {
         return(
-            <div className="list-cards-backlog "></div>
+            <div className="list-cards-backlog ">
+                
+                {
+                    this.state.lista.map((currElement, index) => {
+                        return(
+                            <div key={index}>
+                                <li className="list-group-item">{currElement.nmusuario}</li>
+                            </div>
+                        );
+                    })
+                }
+                
+            </div>
         )
     }
 
